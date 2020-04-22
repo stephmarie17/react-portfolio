8c4da6e7b9354835796d9a7ebb85461165f392a1
Stephanie A <stephanie.m.aurelio@gmail.com>
2020-04-22 16:59:47 -0700

    add netlify contact form

---
+++ src/components/details/ContactForm.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './ContactForm.css';
+
+const ContactForm = () => {
+    return (
+        <div className='contact-form'>
+        <form name="contact" method="post">
+          <input type="hidden" name="form-name" value="contact" />
+          <p>
+            <label>Your Name: <input type="text" name="name"/></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email"/></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message" cols="50" rows="5"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+        </div>
+    )
+}
+
+export default ContactForm
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,31 +1,13 @@
 import React from 'react';
+import ContactForm from '../details/ContactForm';
 import './Contact.css';
 
 const Contact = () => {
   return (
     <div className="contact">
       <h1>Contact Me</h1>
-      <div className="contact-links">
-        <p>
-          <span role="img" aria-label="email me">
-            📨
-          </span>{' '}
-          <a
-            href="mailto:stephanie.m.aurelio@gmail.com?subject=Message via Portfolio"
-            alt="Email"
-          >
-            Email Me
-          </a>
-        </p>
-        <p>
-          <span role="img" aria-label="tweet at me">
-            💬
-          </span>{' '}
-          <a href="https://twitter.com/magicofcode" alt="Twitter">
-            Tweet Me
-          </a>
-        </p>
-      </div>
+      <p>Let's work together! Or, end me a random note.</p>
+        <ContactForm />
     </div>
   );
 };
